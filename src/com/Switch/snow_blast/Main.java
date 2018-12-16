3af00a2f02a0a454f219fdc2506a04c6d8b2b3b5
Joseph Amphlett <joe@amphlett.net>
2018-12-16 00:12:26 +0000

    fireworks added

--- src/com/Switch/snow_blast/Main.java
+++ src/com/Switch/snow_blast/Main.java
@@ -1,14 +1,13 @@
 package com.Switch.snow_blast;
 
-import org.bukkit.ChatColor;
+import org.bukkit.*;
 import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.Player;
-import org.bukkit.entity.Snowball;
-import org.bukkit.entity.Snowman;
+import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.ProjectileHitEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.inventory.meta.FireworkMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.HashMap;
@@ -21,8 +20,9 @@ import java.util.HashMap;
  */
 public class Main extends JavaPlugin implements Listener {
 
-    private HashMap<Player, Long> snowballHitMap = new HashMap<>();
-    private HashMap<Player, Long> freezePlayerMap = new HashMap<>();
+    private HashMap<Player, Long> hitMap = new HashMap<>();
+    private HashMap<Player, Long> freezeMap = new HashMap<>();
+    private HashMap<Player, Long> fireworkMap = new HashMap<>();
 
     @Override
     public void onEnable() {
@@ -33,17 +33,36 @@ public class Main extends JavaPlugin implements Listener {
     @EventHandler
     public void onHit(ProjectileHitEvent event) {
         if (event.getEntity() instanceof Snowball) {
-            Entity target = event.getHitEntity();
-            Entity shooter = (Entity) event.getEntity().getShooter();
-            if (target instanceof Player) {
-                FileConfiguration config = this.getConfig();
-                int action = (int) (Math.random() * 100);
-                if (action <= config.getInt("snowball_freeze.chance")) { // this is a terrible way of doing chances...
-
+            Entity targetEntity = event.getHitEntity();
+            Entity shooterEntity = (Entity) event.getEntity().getShooter();
+            int action = (int) (Math.random() * 100);
+            if (targetEntity instanceof Player) {
+                Player target = (Player) targetEntity;
+                if (action <= actionChance("snowball_freeze.chance")) { // this is a terrible way of doing chances...
+                    targetFreeze(target, shooterEntity);
+                    return;
                 }
-                snowBallHitMessage((Player) target, shooter);
+                if (action <= actionChance("snowball_firework.chance")) {
+                    firework(target, shooterEntity);
+                    return;
+                }
+                snowballHit(target, shooterEntity);
             }
         }
+    }
+
+    private void snowballHit(Player target, Entity shooterEntity) {
+        FileConfiguration config = this.getConfig();
+        if (hitMap.containsKey(target)) {
+            if (hitMap.get(target) + (config.getInt("snowball_hit.cooldown") * 1000) <= System.currentTimeMillis()) {
+                hitMap.remove(target);
+                return;
+            } else {
+                return;
+            }
+        }
+        snowBallHitMessage(target, shooterEntity);
+        hitMap.put(target, System.currentTimeMillis());
     }
 
     private void snowBallHitMessage(Player target, Entity shooterEntity) {
@@ -61,6 +80,88 @@ public class Main extends JavaPlugin implements Listener {
         }
     }
 
+    @EventHandler
+    public void onMove(PlayerMoveEvent event) {
+        Player player = event.getPlayer();
+        if (freezeMap.containsKey(player)) {
+            FileConfiguration config = this.getConfig();
+            if (freezeMap.get(player) + (config.getInt("snowball_freeze.cooldown") * 1000) <= System.currentTimeMillis()) {
+                freezeMap.remove(player);
+                Location location = player.getLocation();
+                location.getBlock().setType(Material.AIR);
+            } else {
+                event.setCancelled(true);
+            }
+        }
+    }
+
+    private void targetFreeze(Player target, Entity shooterEntity) {
+        if (freezeMap.containsKey(target)) {
+            FileConfiguration config = this.getConfig();
+            if (freezeMap.get(target) + (config.getInt("snowball_freeze.cooldown")) >= System.currentTimeMillis()) {
+                freezeMap.remove(target);
+            } else {
+                return;
+            }
+        }
+        freezeMap.put(target, System.currentTimeMillis());
+        Location location = new Location(target.getWorld(),
+                target.getLocation().getBlockX() + 0.5,
+                target.getLocation().getBlockY(),
+                target.getLocation().getBlockZ() + 0.5,
+                target.getEyeLocation().getYaw(),
+                target.getEyeLocation().getPitch());
+        target.getLocation().getBlock().setType(Material.ICE);
+        target.teleport(location);
+        targetFreezeMessage(target, shooterEntity);
+    }
+
+    private void targetFreezeMessage(Player target, Entity shooterEntity) {
+        FileConfiguration config = this.getConfig();
+        if (shooterEntity instanceof Snowman) {
+            String golemMessage = config.getString("snowball_freeze.golem_message");
+            target.sendMessage(translateColorCodes(golemMessage));
+        }
+        else if (shooterEntity instanceof Player) {
+            Player shooter = (Player) shooterEntity;
+            String targetMessage = config.getString("snowball_freeze.target_message");
+            target.sendMessage(translateColorCodes(translatePlaceholder(targetMessage, target, shooter)));
+        }
+    }
+
+    private void firework(Player target, Entity shooterEntity) {
+        if (fireworkMap.containsKey(target)) {
+            FileConfiguration config = this.getConfig();
+            if (fireworkMap.get(target) + (config.getInt("snowball_firework.cooldown") * 1000) <= System.currentTimeMillis()) {
+                fireworkMap.remove(target);
+            } else {
+                return;
+            }
+        }
+        fireworkMap.put(target, System.currentTimeMillis());
+        Firework firework = target.getPlayer().getWorld().spawn(target.getPlayer().getLocation(), Firework.class);
+        FireworkMeta fireworkMeta = firework.getFireworkMeta();
+        fireworkMeta.addEffect(FireworkEffect.builder()
+                .flicker(true).trail(true).withColor(Color.RED, Color.GREEN)
+                .withFade(Color.WHITE).with(FireworkEffect.Type.BALL).build());
+        firework.setFireworkMeta(fireworkMeta);
+        firework.detonate();
+        fireworkMessage(target, shooterEntity);
+    }
+
+    private void fireworkMessage(Player target, Entity shooterEntity) {
+        FileConfiguration config = this.getConfig();
+        if (shooterEntity instanceof Snowman) {
+            String golemMessage = config.getString("snowball_firework.golem_message");
+            target.sendMessage(translateColorCodes(golemMessage));
+        }
+        else if (shooterEntity instanceof Player) {
+            Player shooter = (Player) shooterEntity;
+            String targetMessage = config.getString("snowball_firework.target_message");
+            target.sendMessage(translateColorCodes(translatePlaceholder(targetMessage, target, shooter)));
+        }
+    }
+
     private String translatePlaceholder(String string, Player target, Player shooter) {
         String targetPlaceholder = "%target%";
         String shooterPlaceholder = "%shooter%";
@@ -71,6 +172,19 @@ public class Main extends JavaPlugin implements Listener {
             string = string.replaceAll(shooterPlaceholder, shooter.getDisplayName());
         }
         return string;
+    }
+
+    private int actionChance(String configChancePath) {
+        FileConfiguration config = this.getConfig();
+        String string = configChancePath.substring(0, configChancePath.indexOf('.'));
+        int chance = config.getInt("snowball_freeze.chance");
+        if (!string.equals("snowball_freeze")) {
+            chance += config.getInt("snowball_freeze.chance");
+        } else {return chance;}
+        if (!string.equals("snowball_firework")) {
+            chance += config.getInt("snowball_freeze.chance");
+        } else {return chance;}
+        return chance;
     }
 
     private String translateColorCodes(String string) {
@@ -91,6 +205,12 @@ public class Main extends JavaPlugin implements Listener {
         config.addDefault("snowball_freeze.target_message", "&9%shooter% froze you");
         config.addDefault("snowball_freeze.shooter_message", "&9You froze %target%");
         config.addDefault("snowball_freeze.golem_message", "&9A snowman froze you");
+
+        config.addDefault("snowball_firework.chance", 20);
+        config.addDefault("snowball_firework.cooldown", 3);
+        config.addDefault("snowball_firework.target_message", "&b%shooter% &9hit you with a firework");
+        config.addDefault("snowball_firework.shooter_message", "&9You hit &b%target% &9 a firework");
+        config.addDefault("snowball_firework.golem_message", "&9A &bsnowman &9hit you with a firework");
 
         saveConfig();
 
